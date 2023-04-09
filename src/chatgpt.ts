0ece02338c4638d0e0aa54ee5d6791aac492486c
qiang.liang <173228514@qq.com>
2023-04-09 10:44:07 +0800

    feat:东八区处理

--- src/chatgpt.ts
+++ src/chatgpt.ts
@@ -353,7 +353,7 @@ export class ChatGPTBot {
                 let talkerId = message.talker().id;
                 let date = this.signMap.get(talkerId);
                 let now = new Date();
-                console.log({now: now, ms: now.getTime()})
+                console.log({now: now, ms: now.getTime(), signMap: this.signMap})
                 if (date && date.getDate() == now.getDate()) {
                     const reply = `@${message.talker().name()} 你今天已经抽过签了`;
                     await message.say(reply);
